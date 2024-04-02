1167ae2c56bfd06a55dabd079dec1d63f4f8d1fb
lucabar21 <luca_barbuto@yahoo.it>
2024-04-02 18:19:39 +0200

    first merge commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import TopbarComponent from "./components/TopbarComponent";
 import "./App.css";
-
-// <Recommended title="Persone che potresti conoscere" subtitle="Dal tuo settore" />
-// <Recommended title="Consigliati" />
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import TopbarComponent from "./components/TopbarComponent";
+import HeroSection from "./components/HeroSection";
+import LanguageAndURL from "./components/LanguageAndURL";
+import Recommended from "./components/Recommended";
+import ChatMin from "./components/ChatMin";
+import Analisi from "./components/Analisi";
 
 function App() {
   return (
-    <div className="App">
-      <TopbarComponent />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <TopbarComponent />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Container>
+                <Row>
+                  <Col className="col-9">
+                    <HeroSection />
+                    <Analisi />
+                  </Col>
+                  <Col className="col-3">
+                    <div>
+                      <LanguageAndURL />
+                      <Recommended title="Persone che potresti conoscere" subtitle="Dal tuo settore" />
+                      <Recommended title="Consigliati" />
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            }
+          ></Route>
+        </Routes>
+        <ChatMin />
+      </div>
+    </BrowserRouter>
   );
 }
 
