ba8311b42acbb2234173eb30b20d60577f93738e
MarquisPeroni <marco_p2009@live.it>
2024-04-02 16:11:38 +0200

    firstcommit
    4 new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Analisi from "./components/Analisi";
+import Risorse from "./components/Risorse";
+import Attività from "./components/Attività";
+import Formazione from "./components/formazione";
+import { Container } from "react-bootstrap";
 function App() {
-  return <div className="App"></div>;
+  return (
+  <Container>
+      <Analisi/>
+      <Risorse />
+      <Attività /> 
+      <Formazione/>
+  </Container>
+  )
 }
 
 export default App;
