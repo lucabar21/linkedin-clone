20b8212ede669fe18510b8e0b93e859744d471b0
MarquisPeroni <marco_p2009@live.it>
2024-04-05 10:33:42 +0200

    11commit
    added loginreducer and loginaction

---
+++ src/redux/actions/login.js
@@ -0,0 +1,29 @@
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const login = (username, password) => {
+  return dispatch => {
+    dispatch({ type: LOGIN_REQUEST });
+    
+    fetch('url_del_tuo_server/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Errore durante il login');
+      }
+      return response.json();
+    })
+    .then(data => {
+      dispatch({ type: LOGIN_SUCCESS, payload: data });
+    })
+    .catch(error => {
+      dispatch({ type: LOGIN_FAILURE, payload: error.message });
+    });
+  };
+};
---
+++ src/redux/reducers/loginReducers.js
@@ -0,0 +1,36 @@
+import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE } from '../actions/auth';
+
+const initialState = {
+  loading: false,
+  error: null,
+  isLoggedIn: false,
+  user: null,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isLoggedIn: true,
+        user: action.payload, // Aggiungi eventuali dati dell'utente
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
